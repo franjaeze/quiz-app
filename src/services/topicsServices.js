74f316171d3e5699131d488e156fbb5ebbb4ca75
francisco Verón <fvwear@hotmail.com>
2023-07-15 18:29:11 -0300

    add services

--- src/services/topicsServices.js
+++ src/services/topicsServices.js
@@ -29,7 +29,48 @@ export const topics =[
     {  answer: "An open-source JavaScript library that is used for building user interfaces specifically for single-page applications", correct: false }, 
     {  answer: "Supported by Google, is an open-source, JS-based software engineering platform for building user interfaces (front-end)", correct: false }, 
     {  answer: "A modern JS framework used to build static web apps. You can use It to build single, reusable components for projects of any kind", correct: false },   ]
-}, ]},
+}, 
+
+{
+    question: "Wich one is not a vue directive ?",
+    answers: [  {  answer: "NgFor", correct: true },
+    {  answer: "v-for", correct: false }, 
+    {  answer: "v-bind", correct: false }, 
+    {  answer: "v-model", correct:false },   ]
+    },{
+        question: "Which directive is used for one-way data binding in Vue.js?",
+        answers: [  {  answer: "@click", correct: false },
+        {  answer: "v-for", correct: false }, 
+        {  answer: "v-bind", correct: true }, 
+        {  answer: "v-model", correct: false },   ]
+        },
+        {
+            question: "How is v-show different from the v-if directive in Vue.js",
+            answers: [  {  answer: "Its the same", correct: false },
+            {  answer: "The v-if directive conditionally displays elements, whereas the v-show directive conditionally renders elements.", correct: false }, 
+            {  answer: "The v-show directive conditionally displays elements, whereas the v-if directive conditionally renders elements.", correct: true }, 
+            {  answer: "The v-show its from vue 2, and nowadays its deprecated", correct: false },   ]
+            },
+            {
+                question: "What is Pinia?",
+                answers: [  {  answer: " helps you manage and store reactive data and state across your components in your Vue. js", correct: true },
+                {  answer: "Its a fruit emoji in vue js", correct: false }, 
+                {  answer: "Its the old version of Vuex", correct: false }, 
+                {  answer: "Its vue js router module", correct: false },   ]
+                },
+
+
+
+
+
+
+
+
+
+
+
+
+]},
 
 { name: "JavaScript",
 questions: [{
@@ -55,12 +96,54 @@ answers: [  {  answer: "A library for JavaScript", correct: false },
 {  answer: "Is a standard for scripting languages", correct: true },   ]
 },
 {
-question: "What is Vue?",
-answers: [  {  answer: "An open-source, progressive Javascript framework for building user interfaces that aim to be incrementally adoptable", correct: true },
-{  answer: "An open-source JavaScript library that is used for building user interfaces specifically for single-page applications", correct: false }, 
-{  answer: "Supported by Google, is an open-source, JS-based software engineering platform for building user interfaces (front-end)", correct: false }, 
-{  answer: "A modern JS framework used to build static web apps. You can use It to build single, reusable components for projects of any kind", correct: false },   ]
+question: "Ways to declare a variable.Wich one is wrong?",
+answers: [  {  answer: "const variable ", correct: false },
+{  answer: "let variable", correct: false }, 
+{  answer: "var variable", correct: false }, 
+{  answer: "static variable", correct: true },   ]
 },
+{
+    question: " Which symbol is used for comments in Javascript?",
+    answers: [  {  answer: "// ", correct: true },
+    {  answer: "/-- --/", correct: false }, 
+    {  answer: "!-- --!", correct: false }, 
+    {  answer: "<-- -->", correct: false },   ]
+    },
+    {
+        question: "What are all the types of Pop up boxes available in JavaScript? There is one that does not belong",
+        answers: [  {  answer: "Confirm", correct: false },
+        {  answer: "Pop", correct: true }, 
+        {  answer: "Alert", correct: false }, 
+        {  answer: "Prompt", correct: false },   ]
+        },
+        {
+            question: "How can generic objects be created?",
+            answers: [  {  answer: "var I = Object.type ", correct: false },
+            {  answer: "var I new Object()= Object", correct: false }, 
+            {  answer: "Object I = new Object", correct: false }, 
+            {  answer: "var I = new object()", correct: true },   ]
+            },
+            {
+                question: " Which keywords is not used to handle exceptions?",
+                answers: [  {  answer: "finally", correct: false },
+                {  answer: "catch", correct: false }, 
+                {  answer: "throw", correct: true }, 
+                {  answer: "try", correct: false },   ]
+                },
+                {
+                    question: "How are DOM utilized in JavaScript?",
+                    answers: [  {  answer: "Its JS engine", correct: false },
+                    {  answer: "Is responsible for how various objects in a document interact with each other", correct: true }, 
+                    {  answer: "Connect CSS and html with JS", correct: false }, 
+                    {  answer: "Its an object method", correct: false },   ]
+                    },
+                    {
+                        question: " Which its not a JS type ?",
+                        answers: [  {  answer: "String", correct: false },
+                        {  answer: "Integer", correct: true }, 
+                        {  answer: "Array", correct: false }, 
+                        {  answer: "Object", correct: false },   ]
+                        },
 
 
 
@@ -71,33 +154,76 @@ answers: [  {  answer: "An open-source, progressive Javascript framework for bui
 { name: "Cocktail",
 questions: [{
 
-question: "What is Rum made off?",
-answers: [  {  answer: "Sucar cane", correct: false },
-{  answer: "Potatos", correct: false }, 
-{  answer: "Cereals as malt and wheat", correct: true }, 
+question: "What is Rum made from?",
+answers: [  {  answer: "Sucarcane", correct: true },
+{  answer: "Potatoes", correct: false }, 
+{  answer: "Grains as barley, rye or wheat", correct: false }, 
 {  answer: "Rice", correct: false },   ]
 },
-{
-question: "What is an Aperol",
-answers: [  {  answer: "Cocktial", correct: false },
-{  answer: "Liquor", correct: false }, 
-{  answer: "Spirit", correct: false }, 
-{  answer: "An Aperitif", correct: true },   ]
-},
-{
-question: "Which one of these cocktails its not made with Whsikey",
-answers: [  {  answer: "Old fashioned", correct: false },
-{  answer: "Negroni", correct: false }, 
-{  answer: "Penicilin ", correct: false }, 
-{  answer: "Manhattan", correct: true },   ]
-},
-{
-question: "What is Vue?",
-answers: [  {  answer: "An open-source, progressive Javascript framework for building user interfaces that aim to be incrementally adoptable", correct: true },
-{  answer: "An open-source JavaScript library that is used for building user interfaces specifically for single-page applications", correct: false }, 
-{  answer: "Supported by Google, is an open-source, JS-based software engineering platform for building user interfaces (front-end)", correct: false }, 
-{  answer: "A modern JS framework used to build static web apps. You can use It to build single, reusable components for projects of any kind", correct: false },   ]
-},
+    {
+    question: "What is an Aperol",
+    answers: [  {  answer: "Cocktail", correct: false },
+    {  answer: "Liqueur", correct: false }, 
+    {  answer: "Spirit", correct: false }, 
+    {  answer: "A bitter Aperitif", correct: true },   ]
+    },
+        {
+        question: "Which one of these cocktails it's not made with Whiskey",
+        answers: [  {  answer: "Old fashioned", correct: false },
+        {  answer: "Negroni", correct: true }, 
+        {  answer: "Penicilin ", correct: false }, 
+        {  answer: "Manhattan", correct: false },   ]
+        },
+            {
+            question: "What brand does not belong to gin manufacturers?",
+            answers: [  {  answer: "Beefeater ", correct: false },
+            {  answer: "Belvedere", correct: true }, 
+            {  answer: "Tanqueray ", correct: false }, 
+            {  answer: "Roku", correct: false },   ]
+            },
+                {
+                question: "Which of these cocktails is not native to South America?",
+                answers: [  {  answer: "Caipirinha ", correct: false },
+                {  answer: "Pisco Sour", correct: false }, 
+                {  answer: "Margarita", correct: true }, 
+                {  answer: "Cynar Julep", correct: false },   ]
+                },
+                    {
+                        question: "Which of these ingredients does not belong to the Mojito?",
+                        answers: [  {  answer: "Mint ", correct: false },
+                        {  answer: "Lime", correct: false }, 
+                        {  answer: "Tequila", correct: true }, 
+                        {  answer: "Sparkling water", correct: false },   ]
+                        },
+                            {
+                                question: "What is a Shirley Temple?",
+                                answers: [  {  answer: "A non-alcoholic drink (mocktail)", correct: true },
+                                {  answer: "A cocktail made of Vodka, and fruit syrups", correct: false }, 
+                                {  answer: "One of the bartenders tools", correct: false }, 
+                                {  answer: "A nicknmae for a bar situation", correct: false },   ]
+                                }, 
+                                    {
+                                    question: "What is a Bloody Mary made of?",
+                                    answers: [  {  answer: "Tomatoe juice, salt, pepper, Tabasco, Worcestershire sauce", correct: false },
+                                    {  answer: "Tomtato Juice, Vodka, Tabasco", correct: false }, 
+                                    {  answer: "Vodka, salt, pepper, Tabasco, Tomatoe juice,  Worcestershire sauce", correct: true }, 
+                                    {  answer: "Vodka, salt, pepper, ,  Worcestershire sauce", correct: false },   ]
+                                    },
+                                    {
+                                        question: "Which of these alocholic beverage its not from Asia?",
+                                        answers: [  {  answer: "Sake", correct: false },
+                                        {  answer: "Ouzo", correct: true }, 
+                                        {  answer: "Soju", correct: false }, 
+                                        {  answer: "Baijiu", correct: false },   ]
+                                        },
+
+                                        {
+                                            question: "Which of these is not a type of whiskey",
+                                            answers: [  {  answer: "Bourbon", correct: false },
+                                            {  answer: "Tennessee", correct: false }, 
+                                            {  answer: "Rye", correct: false }, 
+                                            {  answer: "Old Tom", correct: true },   ]
+                                            },
 
 
 
